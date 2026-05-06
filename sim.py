60cbea663f222559d3635c4ad159b405da84f5ad
YanaBaloshenko <ybalyoshenko1405@gmail.com>
2026-05-06 12:41:04 +0200

    classic + data_prep

--- sim.py
+++ sim.py
@@ -11,100 +11,88 @@ from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
 from qiskit_machine_learning.algorithms.classifiers import VQC
 
 import numpy as np
-import pandas as pd
+import datetime
 import time
 from matplotlib import pyplot as plt
-from IPython.display import clear_output
-
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.decomposition import PCA
-from sklearn.model_selection import train_test_split
 
 
-### Initializing simulator ###
-aphrodite = IQMFakeAphrodite() # 54 qubits - not the same as on resonance
-garnet = IQMFakeGarnet() # 20 qubits
-backend = garnet
+def vqc_def(n_features, callback_graph):
 
-sampler = BackendSamplerV2(backend=backend)
+    ### initializing simulator ###
+    aphrodite = IQMFakeAphrodite() # 54 qubits - not the same as on resonance
+    garnet = IQMFakeGarnet() # 20 qubits
+    backend = garnet
 
-### dataset ###
-df_train = pd.read_csv("data/nsl-kdd/KDDTrain+.txt") # reading files
-df_test = pd.read_csv("data/nsl-kdd/KDDTest+.txt")
+    sampler = BackendSamplerV2(backend=backend)
 
-df_train = df_train.head(90).copy()
-df_test = df_test.head(30).copy()
+    ### defining qml ###
+    print("\nFeature map...")
+    feature_map = zz_feature_map(feature_dimension=n_features, reps=1)
+    print("Ansatz...")
+    ansatz = real_amplitudes(num_qubits=n_features, reps=3)
+    print("Oprimizer...")
+    optimizer = SPSA(maxiter=5)#, learning_rate=0.1, perturbation=0.2)
 
-y_train = df_train['attack_type'].apply(lambda x: 0 if x == 'normal' else 1).values # changing to binary
-y_test = df_test['attack_type'].apply(lambda x: 0 if x == 'normal' else 1).values
+    # pass manager for transpilation; optimization_level=3 - qiskit będzie się bardzo starał maksymalnie uprościć obwód
+    print("Transpilation...")
+    pm = generate_preset_pass_manager(target=backend.target, optimization_level=3)
 
-x_train_raw = df_train.drop(['attack_type', 'difficulty_level'], axis=1) # unnecessary columns drop
-x_test_raw = df_test.drop(['attack_type', 'difficulty_level'], axis=1)
+    print("Defining algorithm (VQC)...")
+    vqc = VQC(
+        sampler=sampler,
+        feature_map=feature_map,
+        ansatz=ansatz,
+        optimizer=optimizer,
+        callback=callback_graph,
+        pass_manager=pm
+    )
 
-n_train_samples = len(x_train_raw)
-x_combined = pd.concat([x_train_raw, x_test_raw], axis = 0)
+    return vqc
 
-x_combined_enc = pd.get_dummies(x_combined) # one-hot encoding !!!!!!!!!!!!!!!!!!!!! check
+def main():
+    data = np.load("data/kdd_3.14-scale_5-f.npz")
+    train_features = data['train_features']
+    test_features = data['test_features']
+    train_labels = data['train_labels']
+    test_labels = data['test_labels']
+    n_features = train_features.shape[1]
 
-x_train_enc = x_combined_enc.iloc[:n_train_samples] # getting set back after encoding
-x_test_enc = x_combined_enc.iloc[n_train_samples:]
+    print(f"Number of features: {n_features}")
+    print(f"Number of train records: {train_features.shape[0]}")
+    print(f"Number of test records: {test_features.shape[0]}")
+    print("\nStarting training...")
 
-### normalization ### !!!!!!! check ranges
-scaler = MinMaxScaler(feature_range=(0, np.pi))
-x_train_scaled = scaler.fit_transform(x_train_enc)
-x_test_scaled = scaler.transform(x_test_enc)
+    objective_func_vals = []
+    def callback_graph(weights, value):
+        objective_func_vals.append(value)
+        print(f"Iteracja: {len(objective_func_vals)} | Wartość funkcji celu: {value:.4f}")
 
-### pca ###
-n_features = 4
-pca = PCA(n_components=n_features)
-train_features = pca.fit_transform(x_train_scaled)
-test_features = pca.transform(x_test_scaled)
+    vqc = vqc_def(n_features, callback_graph)
 
-train_labels = y_train # getting labels
-test_labels = y_test
+    start = time.time()
+    vqc.fit(train_features, train_labels)
+    elapsed = time.time() - start
 
-### defining qml ###
-feature_map = zz_feature_map(feature_dimension=n_features, reps=1)
-ansatz = real_amplitudes(num_qubits=n_features, reps=3)
-optimizer = SPSA(maxiter=10, learning_rate=0.05, perturbation=0.1)
+    print(f"\nTraining time: {round(elapsed)} seconds")
 
-# pass manager for transpilation
-# optimization_level=3 - qiskit będzie się bardzo starał maksymalnie uprościć obwód
-pm = generate_preset_pass_manager(target=backend.target, optimization_level=3)
+    # scores - make into normal raport
+    train_score = vqc.score(train_features, train_labels)
+    test_score = vqc.score(test_features, test_labels)
+    print(f"Quantum VQC on the training dataset: {train_score:.2f}")
+    print(f"Quantum VQC on the test dataset: {test_score:.2f}")
 
-objective_func_vals = []
-plt.rcParams["figure.figsize"] = (12, 6)
+    date = datetime.datetime.now()
+    with open(f"results/repots/f{n_features}_{date}.txt", "a") as f:
+        f.write(f"Quantum VQC on the training dataset: {train_score:.2f}")
+        f.write(f"Quantum VQC on the test dataset: {test_score:.2f}")
 
-def callback_graph(n_evals, parameters, value, stepsize, accepted):
-    objective_func_vals.append(value)
-    print(f"Iteracja: {int(n_evals/3)} | Wartość funkcji celu: {value:.4f}")
+    # plot objective function - check out qiskit visualization toolkit
+    plt.rcParams["figure.figsize"] = (12, 6)
+    plt.title("Objective function value against iteration (SPSA)")
+    plt.xlabel("Iteration")
+    plt.ylabel("Objective function value")
+    plt.plot(range(len(objective_func_vals)), objective_func_vals)
+    plt.show()
 
-vqc = VQC(
-    sampler=sampler,
-    feature_map=feature_map,
-    ansatz=ansatz,
-    optimizer=optimizer,
-    callback=callback_graph,
-    pass_manager=pm
-)
-
-# clear objective value history
-objective_func_vals = []
-
-start = time.time()
-vqc.fit(train_features, train_labels)
-elapsed = time.time() - start
-
-print(f"Training time: {round(elapsed)} seconds")
-
-train_score_q4 = vqc.score(train_features, train_labels)
-test_score_q4 = vqc.score(test_features, test_labels)
-
-print(f"Quantum VQC on the training dataset: {train_score_q4:.2f}")
-print(f"Quantum VQC on the test dataset: {test_score_q4:.2f}")
-
-plt.title("Objective function value against iteration (SPSA)")
-plt.xlabel("Iteration")
-plt.ylabel("Objective function value")
-plt.plot(range(len(objective_func_vals)), objective_func_vals)
-plt.show()
+if __name__ == "__main__":
+    main()
