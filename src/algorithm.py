b3a5e054a96ac6db0233b91e00d72200a90b99b5
Yana Baloshenko <ybalyoshenko1405@gmail.com>
2026-05-25 08:26:20 +0200

    added vqr definition

--- src/algorithm.py
+++ src/algorithm.py
@@ -1,21 +1,23 @@
+import os
 import numpy as np
+from dotenv import load_dotenv
 
 from iqm import qiskit_iqm
 from iqm.qiskit_iqm.fake_backends.fake_garnet import IQMFakeGarnet
-from qiskit.primitives import BackendSamplerV2
 from iqm.qiskit_iqm import IQMProvider
+from qiskit.primitives import StatevectorSampler, BackendSamplerV2
+from qiskit.primitives import StatevectorEstimator, BackendEstimatorV2
+from qiskit.quantum_info import SparsePauliOp
 
 from qiskit.circuit.library import zz_feature_map
 from qiskit.circuit.library import real_amplitudes
+from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
+
 from qiskit_machine_learning.optimizers import COBYLA
 from qiskit_machine_learning.optimizers import SPSA
-from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
-from qiskit.primitives import StatevectorSampler
+
 from qiskit_machine_learning.algorithms.classifiers import VQC # https://qiskit-community.github.io/qiskit-machine-learning/stubs/qiskit_machine_learning.algorithms.VQC.html
 from qiskit_machine_learning.algorithms import VQR # https://qiskit-community.github.io/qiskit-machine-learning/stubs/qiskit_machine_learning.algorithms.VQR.html
-
-import os
-from dotenv import load_dotenv
 
 load_dotenv()
 token = os.getenv("IQM_TOKEN")
@@ -24,7 +26,7 @@ iqm_link = "https://resonance.iqm.tech/"
 spsa = SPSA(maxiter=3, learning_rate=0.02, perturbation=0.05) # SPSA doc https://qiskit-community.github.io/qiskit-machine-learning/stubs/qiskit_machine_learning.optimizers.SPSA.html
 
 objective_func_vals = []
-def get_vqc_callback(folder, file_name):
+def get_callback(folder, file_name):
     def callback(*args):
         if len(args) == 5: # SPSA signature: (nfev, weights, value, stepsize, accepted)
             weights = args[1]
@@ -49,31 +51,38 @@ def get_vqc_callback(folder, file_name):
 def backend_def(bcknd):
     if bcknd == "ideal":
         backend=None
+        estimator = StatevectorEstimator()
         sampler = StatevectorSampler()
     elif bcknd == "f_garnet":
-        backend=IQMFakeGarnet()
-        sampler = BackendSamplerV2(backend=backend) # 20 qubits
+        backend=IQMFakeGarnet() # 20 qubits
+        estimator = BackendEstimatorV2(backend=backend)
+        sampler = BackendSamplerV2(backend=backend)
     elif bcknd == "sirius" or bcknd == "garnet" or bcknd == "emerald":
         backend = IQMProvider(iqm_link, quantum_computer=bcknd).get_backend() # docs https://quantum.cloud.ibm.com/docs/en/api/qiskit/qiskit.primitives.BackendSamplerV2
+        
+        estimator = BackendEstimatorV2(backend=backend) # !!!!!!!!!!!!!!!!!!!!!! check options
+        estimator.options.default_precision = 0.05 
+        estimator.options.resilience_level = 1
+        
         sampler = BackendSamplerV2(backend=backend)
         sampler.options.default_shots = 1024
         sampler.options.resilience_level = 1 # resilience_level (int) – level of error mitigation to apply, valid values are 0 (no error mitigation), 1 (basic readout error mitigation), and 2 (advanced error mitigation using quasi-probability method)
     else:
         raise ValueError(f"Couldn't find backend: {bcknd}")
-    return sampler, backend
+    return sampler, estimator, backend
 
 def vqc_def(n_features, bcknd, initial_point, folder, file_name):
     os.makedirs(folder, exist_ok=True)
     os.makedirs(f"{folder}/plots", exist_ok=True)
     os.makedirs(f"{folder}/checkpoints", exist_ok=True)
 
-    sampler, backend = backend_def(bcknd)
+    sampler, _, backend = backend_def(bcknd)
     feature_map = zz_feature_map(feature_dimension=n_features, reps=1, entanglement='linear') # feature map doc https://qiskit-community.github.io/qiskit-machine-learning/stubs/qiskit.circuit.library.ZZFeatureMap.html
     ansatz = real_amplitudes(num_qubits=n_features, reps=1, entanglement='linear') # ansatz doc https://qiskit-community.github.io/qiskit-machine-learning/stubs/qiskit.circuit.library.RealAmplitudes.html
     if hasattr(backend, 'target'):
-        pm = generate_preset_pass_manager(optimization_level=3, target=backend.target) # transpilators docs https://quantum.cloud.ibm.com/docs/en/api/qiskit/transpiler
+        pm = generate_preset_pass_manager(optimization_level=2, target=backend.target) # transpilators docs https://quantum.cloud.ibm.com/docs/en/api/qiskit/transpiler
     else:
-        pm = generate_preset_pass_manager(optimization_level=3) # for ideal backend, no need to specify target as it doesn't have any constraints on gates or connectivity
+        pm = generate_preset_pass_manager(optimization_level=2) # for ideal backend, no need to specify target as it doesn't have any constraints on gates or connectivity
     # pm:
     # optimization_level Level 0 disables all unnecessary optimizations; only transformations needed to make the circuit runnable are used.
     # Level 3 enables a full range of optimization techniques (can be very expensive in compilation time). It is not always guaranteed to produce the best results.
@@ -103,7 +112,7 @@ def vqc_def(n_features, bcknd, initial_point, folder, file_name):
     else:
         optimizer = spsa
 
-    custom_callback = get_vqc_callback(folder, file_name)
+    custom_callback = get_callback(folder, file_name)
 
     print("Defining algorithm (VQC)...")
     vqc = VQC(
@@ -128,20 +137,47 @@ def vqc_def(n_features, bcknd, initial_point, folder, file_name):
 
     return vqc, pm, sampler, backend, objective_func_vals
 
-def vqr_def(n_features, bcknd):
+def vqr_def(n_features, bcknd, initial_point, folder, file_name):
+    os.makedirs(folder, exist_ok=True)
+    os.makedirs(f"{folder}/plots", exist_ok=True)
+    os.makedirs(f"{folder}/checkpoints", exist_ok=True)
 
+    _, estimator, backend = backend_def(bcknd) 
+
+    if bcknd == "ideal":
+        optimizer = SPSA(maxiter=40)
+    else:
+        optimizer = spsa
+
+    feature_map = zz_feature_map(feature_dimension=n_features, reps=1, entanglement='linear')
+    ansatz = real_amplitudes(num_qubits=n_features, reps=1, entanglement='linear')
+    
+    observable = SparsePauliOp.from_list([("Z" * n_features, 1)]) # observable !!!!!!!!!!!!!!!!! check
+
+    if hasattr(backend, 'target'):
+        pm = generate_preset_pass_manager(optimization_level=2, target=backend.target)
+    else:
+        pm = generate_preset_pass_manager(optimization_level=2)
+
+    custom_callback = get_callback(folder, file_name)
+
+    print("Defining algorithm (VQR)...")
     vqr = VQR(
         #num_qubits (int | None) – for the underlying QNN, if None - derived from the feature map or ansatz
-        #feature_map (QuantumCircuit | None) – zz_feature_map() is default, for a single qubit regression problem - z_feature_map()
-        #ansatz (QuantumCircuit | None) – real_amplitudes() is default
-        #observable (BaseOperator | None) – observable to be measured in the underlying QNN
+        feature_map=feature_map, # zz_feature_map() is default, for a single qubit regression problem - z_feature_map()
+        ansatz=ansatz, # real_amplitudes() is default
+        observable=observable, # (BaseOperator | None) observable to be measured in the underlying QNN
         #loss (str | Loss) – default is squared error
-        #optimizer (Optimizer | Minimizer | None) – defaults to SLSQP
+        optimizer=optimizer, # defaults to SLSQP
         #warm_start (bool) – use weights from previous fit to start next fit
-        #initial_point (np.ndarray | None) – for the optimizer to start from
-        #callback (Callable[[np.ndarray, float], None] | None) – has two parameters and returns None, each iteration optimizer invokes callback and passes current weights as array and computed value as float of the objective function
-        #estimator (BaseEstimatorV2 | None) (optional)
-        #pass_manager (BasePassManager | None)
+        initial_point=initial_point,
+        callback=custom_callback,
+        estimator=estimator,
+        pass_manager=pm
     )
 
-    # return vqr, pm, sampler, backend, objective_func_vals
+    vqr.feature_map.draw(output='mpl').savefig(f"{folder}/plots/{file_name}_featuremap.png", dpi=300, bbox_inches='tight')
+    vqr.ansatz.draw(output='mpl').savefig(f"{folder}/plots/{file_name}_ansatz.png", dpi=300, bbox_inches='tight')
+    vqr.circuit.draw(output='mpl').savefig(f"{folder}/plots/{file_name}_complete-circuit.png", dpi=300, bbox_inches='tight')
+
+    return vqr, pm, estimator, backend, objective_func_vals
