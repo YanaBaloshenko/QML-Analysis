f61c5581b27626e708b27a3e4af551b3cd35f3e9
Yana Baloshenko <ybalyoshenko1405@gmail.com>
2026-06-14 00:11:50 +0200

    qpu ready

--- src/vqc_qpu.py
+++ src/vqc_qpu.py
@@ -49,16 +49,9 @@ def results_save(ml, backend, sampler, bcknd, objective_func_vals, folder, train
         json.dump(metadata, f, indent=4)
 
     print("Generating plots...")
-    if compiled_base_circuit is not None:
-        compiled_base_circuit.draw(output='mpl', idle_wires=False).savefig(f"{folder}/{filename}_transpiled-circuit.png", dpi=300)
-    else:
-        print("No circuit, skipping transpilation plot...")
-    if backend is not None and compiled_base_circuit is not None:
-        plot_circuit_layout(compiled_base_circuit, backend).savefig(f"{folder}/{filename}_hardware-layout.png", dpi=300)
-        plot_error_map(backend).savefig(f"{folder}/{filename}_error-map.png", dpi=300)
-    else:
-        print("No hardware backend, skipping layout plot...")
-    
+    compiled_base_circuit.draw(output='mpl', idle_wires=False).savefig(f"{folder}/{filename}_transpiled-circuit.png", dpi=300)
+    plot_circuit_layout(compiled_base_circuit, backend).savefig(f"{folder}/{filename}_hardware-layout.png", dpi=300)
+    plot_error_map(backend).savefig(f"{folder}/{filename}_error-map.png", dpi=300)
     if objective_func_vals: # objective function plot
         plt.figure()
         plt.rcParams["figure.figsize"] = (12, 6)
@@ -70,7 +63,24 @@ def results_save(ml, backend, sampler, bcknd, objective_func_vals, folder, train
 
     plt.close('all') # RAM cleaning
 
-def training(bcknd, train_features, train_labels, test_features, n_features, d_file, folder, filename, pre_t):
+def training(bcknd, d_file):
+    d_path = f"dataset/{d_file}"
+    date = datetime.datetime.now().strftime("%d%m%Y_%H%M%S")
+    r_folder = "results/qpu/vqc"
+    filename = f"vqc_data-{d_file}_backend-{bcknd}_time-{date}"
+    folder = f"{r_folder}/{filename}"
+    os.makedirs(folder, exist_ok=True)
+    os.makedirs(f"{folder}/checkpoints", exist_ok=True)
+
+    ################## data read ##################
+    data = np.load(f"{d_path}/{d_file}.npz")
+    train_features = data['train_features']
+    train_labels = data['train_labels']
+    test_features = data['test_features']
+    n_features = train_features.shape[1]
+
+    ################## training and saving results ##################
+    print("Starting training...")
     ml, pm, sampler, backend, objective_func_vals = qpu_algorithm.vqc_def(n_features, bcknd, folder, filename)
     
     start = time.time()
@@ -89,34 +99,13 @@ def training(bcknd, train_features, train_labels, test_features, n_features, d_f
 
     results_save(ml, backend, sampler, bcknd, objective_func_vals, folder, train_time, n_features, d_file, filename, compiled_base_circuit, test_features)
 
-def prep(bcknd, d_file, pre_t):
-    d_path = f"dataset/{d_file}"
-    date = datetime.datetime.now().strftime("%d%m%Y_%H%M%S")
-    r_folder = "results/qpu/vqc"
-    filename = f"vqc_data-{d_file}_backend-{bcknd}_time-{date}"
-    folder = f"{r_folder}/{filename}"
-    os.makedirs(folder, exist_ok=True)
-    os.makedirs(f"{folder}/checkpoints", exist_ok=True)
-
-    ################## data read ##################
-    data = np.load(f"{d_path}/{d_file}.npz")
-    train_features = data['train_features']
-    train_labels = data['train_labels']
-    test_features = data['test_features']
-    n_features = train_features.shape[1]
-
-    ################## training and saving results ##################
-    print("Starting training...")
-    training(bcknd, train_features, train_labels, test_features, n_features, d_file, folder, filename, False)
- 
-
 def main():
-    bcknd = "ideal"
+    bcknd = "sirius"
     d_n = 5
     d_size = 240
     d_file = f"kdd_3.14-scale_{d_n}-fpca_onehot-enc_{d_size}"
 
-    prep(bcknd, d_file)
+    training(bcknd, d_file)
     
 if __name__ == "__main__":
     main()
